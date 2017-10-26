6ebe643f1e08be0e24754e17976e7fb07bda8f56
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-10-26 21:07:46 +0200

    forbidden

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var serverChatService = require('../server/chatService');
 var router = express.Router();
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
+  verif = serverChatService.authenticate(req);
+  if(verif) res.status(200).send(req.query['hub.challenge']);
+  else     res.sendStatus(403);
   res.render('index');
 });
 
